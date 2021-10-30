2c56e9ce6d494b0852dbf48b6c9f29e3260a25ec
Yusuf Taiwo Hassan <teehazzan@gmail.com>
2021-10-30 21:13:14 +0100

    added swap

--- src/components/Swap.js
+++ src/components/Swap.js
@@ -1,39 +1,73 @@
-import React from "react";
+import React, { useState } from "react";
 import "./swap.css";
 import { ArrowDown, SettingsIcon } from "./Icons";
 import EthIcon from "../images/download.png";
 function Swap() {
+  const [aliasToAddress, setaliasToAddress] = useState(true);
+
+  const swapfields = () => {
+    setaliasToAddress(!aliasToAddress)
+  }
   return (
     <div className="swap-container">
       <div className="swap-contain">
         <div className="swap-contain_top">
-          <div>swap</div>
+          <div>Inquiry</div>
           <SettingsIcon />
         </div>
         <div className="swap-contain-top">
           <div className="swap_input-contain">
-            <div className="swap_input">
-              <div className="swap-internal">
-                <div className="swap_token">
-                  <img src={EthIcon} width={24} alt="" />
-                  <span>ETH</span>
+            {
+              aliasToAddress &&
+
+              <div className="swap_input">
+                <div className="swap-internal">
+                  <div className="swap_token">
+                    <img src={EthIcon} width={24} alt="" />
+                    <span>ETH</span>
+                  </div>
+
+                  <input placeholder="0.0" className="swap_value" />
                 </div>
-
-                <input placeholder="0.0" className="swap_value" />
-                {/* <div className="swap_value">00:00</div> */}
               </div>
-            </div>
-            <div className="middleArrow">
-              <ArrowDown />
-            </div>
-            <div className="swap_input">
-              <div className="swap-internal">
-                <div className="swap_token">ETH</div>
+            }
+            {
+              !aliasToAddress &&
 
-                {/* <div className="swap_value">00:00</div> */}
-                <input placeholder="0.0" className="swap_value" />
+              <div className="swap_input">
+                <div className="swap-internal">
+                  <div className="swap_token">ETH</div>
+                  <input placeholder="0.0" className="swap_value" />
+                </div>
               </div>
+            }
+            <div className="middleArrow" onClick={()=>swapfields()}>
+              <ArrowDown  />
             </div>
+            {
+              !aliasToAddress &&
+
+              <div className="swap_input">
+                <div className="swap-internal">
+                  <div className="swap_token">
+                    <img src={EthIcon} width={24} alt="" />
+                    <span>ETH</span>
+                  </div>
+
+                  <input placeholder="0.0" className="swap_value" />
+                </div>
+              </div>
+            }
+            {
+              aliasToAddress &&
+
+              <div className="swap_input">
+                <div className="swap-internal">
+                  <div className="swap_token">ETH</div>
+                  <input placeholder="0.0" className="swap_value" />
+                </div>
+              </div>
+            }
           </div>
 
           <div className="connect-btn">Connect wallet</div>
