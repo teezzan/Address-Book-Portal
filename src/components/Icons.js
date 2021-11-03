d4cac6a724505733dc11a8d16073d8dc75aa9bf9
Yusuf Taiwo Hassan <teehazzan@gmail.com>
2021-11-03 10:53:57 +0100

    added modal

--- src/components/Icons.js
+++ src/components/Icons.js
@@ -34,3 +34,21 @@ export const ArrowDown = () => (
     <polyline points="19 12 12 19 5 12"></polyline>
   </svg>
 );
+
+
+
+export const Account = () => (
+
+  <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="sc-1ir7xlr-10 gQfzqM"><line x1="18" y1="6" x2="6" y2="18"></line><line x1="6" y1="6" x2="18" y2="18"></line></svg>
+)
+
+
+export const Copy =()=>(
+
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><rect x="9" y="9" width="13" height="13" rx="2" ry="2"></rect><path d="M5 15H4a2 2 0 0 1-2-2V4a2 2 0 0 1 2-2h9a2 2 0 0 1 2 2v1"></path></svg>
+)
+
+
+export const View =()=>(
+  <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M18 13v6a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V8a2 2 0 0 1 2-2h6"></path><polyline points="15 3 21 3 21 9"></polyline><line x1="10" y1="14" x2="21" y2="3"></line></svg>
+)
--- src/components/nav.js
+++ src/components/nav.js
@@ -1,54 +1,111 @@
-import React from "react";
+import React, { useState } from "react";
 import "./nav.css";
 import Icon from "../images/icon.svg";
-function nav(props) {
+import Modal from 'simple-react-modal'
+import { Account , Copy, View} from "./Icons";
+
+function Nav(props) {
+  const [show, setShow] = useState(false)
 
   return (
-    <div className="nav-bar">
-      <div className="internal-nav">
-        <a href="." className="logo">
-          <div>
-            <img src={Icon} width={25} alt={"l"} />
+    <>
+      <div className="nav-bar">
+        <div className="internal-nav">
+          <a href="." className="logo">
+            <div>
+              <img src={Icon} width={25} alt={"l"} />
+            </div>
+          </a>
+          <div className="mid-pill mid-nav" style={{
+            transform: "translateX(135px)"
+          }}>
+            <a className={`${props.task === "Inquiry" ? "pill ACTIVE" : "pill"} `}
+              href="#/inquiry"
+              onClick={() => { props.setTask("Inquiry") }}
+              aria-current="page">
+              Inquiry
+            </a>
+            <a className={` ${props.task === "Send" ? "pill ACTIVE" : "pill"} `}
+              id="pool-nav-link"
+              onClick={() => { props.setTask("Send") }}
+              href="#/send"
+            >
+              Send Ether
+            </a>
+            <a className="pill"
+              id="pool-nav-link"
+              href="https://github.com/teezzan/Adress-Book-Portal">
+              Github<sup>↗</sup>
+            </a>
           </div>
-        </a>
-        <div className="mid-pill mid-nav" style={{
-          transform: "translateX(135px)"
-        }}>
-          <a className={`${props.task === "Inquiry" ? "pill ACTIVE" : "pill"} `}
-            href="#/inquiry"
-            onClick={() => { props.setTask("Inquiry") }}
-            aria-current="page">
-            Inquiry
-          </a>
-          <a className={` ${props.task === "Send" ? "pill ACTIVE" : "pill"} `}
-            id="pool-nav-link"
-            onClick={() => { props.setTask("Send") }}
-            href="#/send"
-          >
-            Send Ether
-          </a>
-          <a className="pill"
-            id="pool-nav-link"
-            href="https://github.com/teezzan/Adress-Book-Portal">
-            Github<sup>↗</sup>
-          </a>
-        </div>
-        <div className="network-add">
-          <div className="mid-pill">
-            <div className="pill ACTIVE" aria-current="page">
-              @{props.user.alias}
-            </div>
-            <div className="pill" id="pool-nav-link" href="#/pool">
-              {props.user.address}
-            </div>
-            <div className="pill" id="pool-nav-link" href="#/pool">
-              {props.user.balance} ETH
+          <div className="network-add">
+            <div className="mid-pill">
+              <div className="pill ACTIVE" aria-current="page">
+                @{props.user.alias}
+              </div>
+              <div className="pill" id="pool-nav-link" href="#/pool" onClick={() => setShow(!show)}>
+                {props.user.address}
+              </div>
+              <div className="pill" id="pool-nav-link" href="#/pool">
+                {props.user.balance} ETH
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+
+      <Modal show={show} onClose={() => setShow(!show)} className="themodal">
+        <div className="modal__content">
+
+          <div className="content__top">
+
+            <div>
+              Account
+            </div>
+            <div 
+            onClick={() => setShow(!show)}
+
+            style={{
+              cursor:"pointer"
+            }}
+            >
+            <Account  />
+
+            </div>
+          </div>
+
+          <div className="modal__account">
+
+            <div className="modal__account-contain">
+              <div className="contain__item">
+                <div className="contain__item-first">
+                  Connected with Metamask
+                </div>
+
+                <div className="contain__item-sec">
+                  Change
+                </div>
+              </div>
+              <div className="contain__item addr">
+                <div className="addr">
+                  0x6D5F...F69E
+                </div>
+              </div>
+              <div className="contain__item">
+                <div className="copy_addr">
+                 <Copy/> Copy Address
+                </div>
+
+                <div className="view_addr">
+                  <View/>View on Explorer
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Modal>
+    </>
   );
 }
 
-export default nav;
+export default Nav;
